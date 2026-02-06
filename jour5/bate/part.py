5127bc2575d4ce99c5fb53c71dbf9e615a55dae1
ceciliaperana13 <cperana13@gmail.com>
2026-02-06 12:01:45 +0100

    main a faire

--- jour5/bate/part.py
+++ jour5/bate/part.py
@@ -1,16 +1,17 @@
 
 
-
 class Part:
-    def __init__(self,name,material):
-        self.name=name
-        self.material=material
-    def getName(self):
-        return self.name
-    def getmaterial(self):
-        return self.material
-    def changeMaterial(self,material):
-        self.material=material
+    """Classe représentant une pièce de bateau"""
+    
+    def __init__(self, name, material):
+        self.name = name
+        self.material = material
+    
+    def change_material(self, new_material):
+        """Modifie le matériau de la pièce"""
+        old_material = self.material
+        self.material = new_material
+        return old_material
+    
     def __str__(self):
-        return f"Part(name={self.name}, material={self.material})"
-        
+        return f"{self.name} en {self.material}"
